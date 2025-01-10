9c266ead4f90c1942a911db6e7b4af8feb4499ff
BinattiLeonardo <142456013+BinattiLeonardo@users.noreply.github.com>
2025-01-09 21:19:11 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,14 +2,18 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-    
-    if(imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.textContent = 'Alugar';
-        botao.classList.remove('dashboard__item__button--return');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        }
     } else {
         imagem.classList.add('dashboard__item__img--rented');
-        botao.textContent = 'Devolver';
         botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
     }
 }
